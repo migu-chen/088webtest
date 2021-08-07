f0598ddd433a559499930948abbc04c7c916643d
migu-chen <w123640a@gmail.com>
2021-08-07 12:09:06 +0800

    pro init

---
+++ WebformMiniSample/AccountingNote.DBsource/DBHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingNote.DBsource
+{
+    class DBHelper
+    {
+        public static string GetConnectionString()
+        {
+            string val =
+                ConfigurationManager.ConnectionStrings
+                ["DefaultConnection"].ConnectionString;
+            return val;
+        }
+
+        public static DataTable ReadDataTable(string connStr, string dbCommand, List<SqlParameter> list)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlCommand comm = new SqlCommand(dbCommand, conn))
+                {
+                    //comm.Parameters.AddWithValue("@userID", userID);
+                    comm.Parameters.AddRange(list.ToArray());
+
+                    conn.Open();
+                    var reader = comm.ExecuteReader();
+
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+
+                    return dt;
+
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ WebformMiniSample/AccountingNote.DBsource/logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingNote.DBsource
+{
+    public  class logger
+    {
+        public static void WriteLog(Exception ex)
+        {
+            string Msg =
+                $@"{ DateTime .Now .ToString ("yyyy-MM-dd HH:mm:ss")  }
+                  {ex.ToString ()}  ";
+            System.IO.File.AppendAllText("D:\\logs\\logs.log", ex.ToString());
+            throw ex;
+        }
+    }
+}
